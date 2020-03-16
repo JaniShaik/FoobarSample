64afe8cc3ae9e84ed6fbf0ae62150b872ae6946c
veera <veeraprasadvellaturi@gmail.com>
2020-03-16 23:41:14 +0530

    @veera first commit

---
+++ app/src/main/java/com/highgo/restaurentapp/activity/SelectionActivity.java
@@ -0,0 +1,40 @@
+package com.highgo.restaurentapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.highgo.restaurentapp.R;
+
+public class SelectionActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_selection);
+
+        Button btBar = findViewById(R.id.btBar);
+        btBar.setOnClickListener(this);
+
+        Button btRest = findViewById(R.id.btRest);
+        btRest.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btBar:
+                startActivity(new Intent(SelectionActivity.this,MainActivity.class));
+                break;
+            case R.id.btRest:
+                startActivity(new Intent(SelectionActivity.this,LoginActivity.class));
+                break;
+        }
+    }
+}
